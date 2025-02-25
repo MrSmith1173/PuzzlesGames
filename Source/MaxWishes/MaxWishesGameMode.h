d36784d56ffd5d5d9e7d9c1c2d292f9e5942f781
MrSmith1173 <matthew.smith201@yahoo.com>
2025-02-25 12:27:39 -0500

    fixed

---
+++ Source/MaxWishes/MaxWishesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MaxWishesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMaxWishesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMaxWishesGameMode();
+};
+
+
+
